205c73bcc4fc33c6e8784a6d207c7d5702b68562
Ajay Bhagchandani <ajay.b@servify.in>
2020-10-30 01:39:51 +0530

    Added staticroot

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
